0456344dd1dc2bb677cec0a832a4db4d650ddd93
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-11 18:51:30 +0200

    upshift logic

--- rpm_sim.ino
+++ rpm_sim.ino
@@ -1,17 +1,36 @@
 int leds[] = {2,3,4,5,6,7};
+int upshift_button = 13;
+
+int gear = 1;
+bool lastState = HIGH;
+bool currentState;
 
 void setup() {
   for(int i = 0; i < 7; i++)
     pinMode(leds[i], OUTPUT);
-
+  pinMode(upshift_button, INPUT_PULLUP);
+  Serial.begin(9600);
+  for(int i = 1; i <= 3; ++i)
+  {
+    for(int i = 0; i < 7; i++)
+      digitalWrite(leds[i], HIGH);
+    delay(300);
+    for(int i = 0; i < 7; ++i)
+      digitalWrite(leds[i], LOW);
+    delay(300);
+  }
 }
 
 void loop() {
-  for(int i = 0; i < 7; i++)
-  {
-    digitalWrite(leds[i],HIGH);
-    delay(1000);
-    digitalWrite(leds[i], LOW);
-    delay(1000);
+  currentState = digitalRead(upshift_button);
+  if(currentState == LOW && lastState == HIGH && gear < 5){
+      gear++;
+      Serial.println(gear);
+      lastState = LOW;
+      delay(50);
   }
+  else{
+    lastState = currentState;
+  }
+
 }
